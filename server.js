90a02b2b7f0a2176d62deab9f3981e6de5a57b13
jonahgalesloot <jonahgalesloot@bsnstudent.nl>
2025-07-22 12:24:04 +0000

    landing page

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const path = require('path');
+
+// serve login page at /login
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'login.html'));
+});
+
+// serve dashboard at /dashboard
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'dashboard.html'));
+});
+
+// serve dashboard at /dashboard
+app.get('/landing', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'landing.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 
